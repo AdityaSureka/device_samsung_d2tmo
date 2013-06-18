76d21a4bfcf70563362448ec7d235145c1b5e115
itsmikeramsay <ronaldramsayii@gmail.com>
2013-06-18 10:14:30 -0400

    Illusion ready...
    Change-Id: I9ab91e941e1ca6a38cc29b55292bf6599aa69eaa

---
+++ illusion.mk
@@ -0,0 +1,28 @@
+$(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := d2tmo
+
+# Inherit some common illusion stuff.
+$(call inherit-product, vendor/illusion/config/gsm.mk)
+
+# Inherit some common illusion stuff.
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/illusion/config/common_sgs.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/illusion/config/common_ledflash.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/illusion/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2tmo TARGET_DEVICE=d2tmo BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.1.1/JRO03L/T999UVDLJA:user/release-keys"
+PRIVATE_BUILD_DESC="d2tmo-user 4.1.1 JRO03L T999UVDLJA release-keys"
+
+PRODUCT_NAME := illusion_d2tmo
+PRODUCT_DEVICE := d2tmo
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SAMSUNG-SGH-I747
+PRODUCT_MANUFACTURER := Samsung
